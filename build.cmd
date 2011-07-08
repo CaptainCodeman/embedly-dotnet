440947e33e03191ed89972bb69a93b8594a92d20
Simon Green <simon@captaincodeman.com>
2011-07-07 20:43:06 -0600

    Simple build script and initial NuGet packaging

---
+++ build.cmd
@@ -0,0 +1,8 @@
+rem @echo off
+set FRAMEWORK_PATH=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319
+set PATH=%PATH%;%FRAMEWORK_PATH%;
+
+msbuild /nologo src/embedly-dotnet.sln /p:Configuration=Release /t:Clean
+msbuild /nologo src/embedly-dotnet.sln /p:Configuration=Release /p:TargetFrameworkVersion=4.0
+
+pkg\nuget Pack pkg\embedly-dotnet.nuspec
