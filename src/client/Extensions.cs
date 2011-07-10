674876f7b97f05b893f77070a9d4035d422f8424
CaptainCodeman <simon@captaincodeman.com>
2011-07-10 15:27:11 -0600

    Code comments

--- src/client/Extensions.cs
+++ src/client/Extensions.cs
@@ -20,11 +20,28 @@ namespace Embedly
 			return source.Select(url => new UrlRequest(client.GetProvider(url), url));
 		}
 
+		/// <summary>
+		/// Extension to enable Urls to be filteres based on the provider.
+		/// </summary>
+		/// <remarks>
+		/// The reason this is used instead of a .Where() on the final result is that we want to filter
+		/// the urls before they are sent to embedly in a request
+		/// </remarks>
+		/// <param name="source">The source.</param>
+		/// <param name="predicate">The predicate.</param>
+		/// <returns></returns>
 		internal static IEnumerable<UrlRequest> WhereProvider(this IEnumerable<UrlRequest> source, Func<Provider, bool> predicate)
 		{
 			return predicate == null ? source : source.Where(pr => predicate(pr.Provider));
 		}
 
+		/// <summary>
+		/// Chunks the source into batches.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source">The source.</param>
+		/// <param name="size">The size.</param>
+		/// <returns></returns>
 		internal static IEnumerable<IEnumerable<T>> TakeChunks<T>(this IEnumerable<T> source, int size)
 		{
 			var list = new List<T>(size);
